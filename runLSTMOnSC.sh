0a78f173e5d3bf95b2deb0deaca3aa213e00af3a
aaron777collins <aaron777collins@gmail.com>
2022-12-06 21:52:44 -0500

    moved time to 6 hours

--- runLSTMOnSC.sh
+++ runLSTMOnSC.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -J ModelTrainerSortedByTimeLSTM
 #SBATCH --account=def-arunita
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=128G # 128GiB of memery
-#SBATCH -t 0-03:0 # Running time of 10 min is 0-00:10
-~/ENV/bin/activate
+#SBATCH -t 0-06:0 # Running time of 10 min is 0-00:10
+. ~/ENV/bin/activate
 python ModelTrainerSortedByTimeLSTM.py
