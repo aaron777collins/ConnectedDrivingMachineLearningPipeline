0f9de754235ddfed72b882340a229f7f528d7e86
aaron777collins <aaron777collins@gmail.com>
2022-12-08 16:58:15 -0500

    added more memory

--- runLSTMOnSC.sh
+++ runLSTMOnSC.sh
@@ -2,7 +2,7 @@
 #SBATCH -J ModelTrainerSortedByTimeLSTM
 #SBATCH --account=def-arunita
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=256G # 256GiB of memery
-#SBATCH -t 0-06:0 # Running time of 10 min is 0-00:10
+#SBATCH --mem=512G # 512GiB of memery
+#SBATCH -t 0-12:0 # Running time of 10 min is 0-00:10
 . ~/ENV/bin/activate
 python ModelTrainerSCSortedByTimeLSTM.py
