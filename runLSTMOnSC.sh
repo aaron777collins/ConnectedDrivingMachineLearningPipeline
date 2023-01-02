af1e4247112808517b79370b4b24deab3dcedaab
aaron777collins <aaron777collins@gmail.com>
2023-01-02 18:30:29 -0500

    changed lstm script

--- runLSTMOnSC.sh
+++ runLSTMOnSC.sh
@@ -2,7 +2,7 @@
 #SBATCH -J ModelTrainerSortedByTimeLSTM
 #SBATCH --account=def-arunita
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=750G # 750GiB of memery
+#SBATCH --mem=256G # 256GiB of memery
 #SBATCH -t 0-12:0 # Running time of 10 min is 0-00:10
 . ~/ENV/bin/activate
 python ModelTrainerSCSortedByTimeLSTM.py
